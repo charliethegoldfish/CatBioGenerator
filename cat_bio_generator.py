8e796a56cfc51c954718a01c9f4392a211e38ff9
Charlie Francis Cassidy <charliefrancis.cassidy@gmail.com>
2021-12-27 17:40:53 +1100

    Commenting in basic structure

---
+++ cat_bio_generator.py
@@ -0,0 +1,15 @@
+# Imports I need
+
+# Variables
+
+# Function to read in files and store data
+
+# Function to pick data to fill in bio with
+
+# Function to construct bio
+
+# Function to display bio (should this be combined with above function?)
+
+# Function to take input
+
+# Main code goes below here
