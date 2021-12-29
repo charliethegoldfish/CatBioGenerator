d99fb1df4be9beac493dff4b6ac0229ebf96b9a4
Charlie Francis Cassidy <charliefrancis.cassidy@gmail.com>
2021-12-29 11:52:17 +1100

    Basic bio generator functionality
    - Added four lists to cover name, colour, adjective and interests
    - Added bio structure string
    - Added function to take input then display the bio
    - Added function to construct the bio

--- cat_bio_generator.py
+++ cat_bio_generator.py
@@ -1,15 +1,42 @@
-# Imports I need
+import random
 
-# Variables
+# Eventually these will be filled from some text files
+cat_names = ["Rufus", "Iggy", "Caramel", "Popcorn"]
+cat_colours = ["black", "red", "aquamarine"]
+cat_adjectives = ["fluffy", "sly", "bouncey", "graceful"]
+cat_interests = ["hiking", "stealing snacks", "fetch", "standing in the shower"]
+
+bio_structure = "{name} is a {adjective} {colour} cat who enjoys {activity_01} and {activity_02}"
 
 # Function to read in files and store data
 
 # Function to pick data to fill in bio with
+def pick_element(list):
+    index = random.randint(0, len(list)-1)
+    return list[index]
 
 # Function to construct bio
+def construct_bio():
+    name = pick_element(cat_names)
+    colour = pick_element(cat_colours)
+    adj = pick_element(cat_adjectives)
+    activity_01 = pick_element(cat_interests)
+    activity_02 = pick_element(cat_interests)
+
+    return bio_structure.format(name=name, colour=colour, adjective=adj, activity_01=activity_01, activity_02=activity_02)
 
 # Function to display bio (should this be combined with above function?)
 
 # Function to take input
+def display_greeting():
+    print("Welcome to the Digital Cat Shelter. Would you like to read the bio for one of our cats? Y/N")
+    answer = input()
+
+    if answer.lower() == 'y':
+        # print("CAT BIO HERE")
+        print(construct_bio())
+    else:
+        print("Have a nice day!")
 
 # Main code goes below here
+display_greeting()
